69584f0cd479abb207822c7f9ba1d886c1160e3c
lulu-sawaf <123410787+lulu-sawaf@users.noreply.github.com>
2025-03-24 13:52:27 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,3 +5,5 @@ from main import *
 ## Feel free to add your own tests here.
 def test_multiply():
     assert quadratic_multiply(BinaryNumber(3), BinaryNumber(3)) == 3*3
+    assert quadratic_multiply(BinaryNumber(18), BinaryNumber(18)) == 18*18
+    assert quadratic_multiply(BinaryNumber(7), BinaryNumber(7)) == 7*7
